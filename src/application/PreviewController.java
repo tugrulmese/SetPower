3ed541d3bef2051df8b2298a93661b638d8b61be
tugrulmese <tugrulmesee@gmail.com>
2017-11-15 14:51:40 +0300

    update

--- src/application/PreviewController.java
+++ src/application/PreviewController.java
@@ -1,15 +1,22 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class PreviewController {
 
+	public String hudsonPramAdress = "C:\\SetPower\\hudson.params";
 	@FXML
 	private TextArea hudsonTextArea;
 
@@ -32,6 +39,7 @@ public class PreviewController {
 		stage.close();
 	}
 
+	@SuppressWarnings("unused")
 	@FXML
 	void onWrite(ActionEvent event) {
 
@@ -48,15 +56,52 @@ public class PreviewController {
 		}
 
 		else {
-			controller.writeNewFile(text);
+
+			File f = new File(hudsonPramAdress);
+			if (f.exists() && !f.isDirectory()) {
+
+				try (BufferedReader reader = new BufferedReader(new FileReader(new File("hudson.params")))) {
+
+					String line;
+					while ((line = reader.readLine()) == null)
+						
+
+
+					if (line != null) {
+						Alert alert = new Alert(AlertType.CONFIRMATION);
+						alert.setTitle("File is available");
+						alert.setHeaderText("There is a file with the same name");
+						alert.setContentText("Do you write on it?");
+
+						Optional<ButtonType> result = alert.showAndWait();
+						if (result.get() == ButtonType.OK) {
+
+							controller.writeNewFile(text);
+						}
+					
+						alert.close();
+						
+					} else {
+
+						controller.writeNewFile(text);
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+
+				controller.writeNewFile(text);
+			}
+
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Writing completed");
+			alert.setHeaderText(null);
+			alert.setContentText("Hudson Params file is created.");
+
+			alert.showAndWait();
+
 		}
 
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setTitle("Writing completed");
-		alert.setHeaderText(null);
-		alert.setContentText("Hudson Params file is created.");
-
-		alert.showAndWait();
 	}
-
 }
--- src/application/SetPowerMainController.java
+++ src/application/SetPowerMainController.java
@@ -333,8 +333,6 @@ public class SetPowerMainController {
 
 	}
 
-	
-
 	@FXML
 	void onConnect(ActionEvent event) {
 
@@ -516,55 +514,34 @@ public class SetPowerMainController {
 		return setData;
 	}
 
-	
 	private String createPreviewText() {
 		String result = "";
 		result = "jaf_branch_name=" + jafBranchText.getText() + "\n";
-		result = result + "prod_branch_name=" + prodBranchText.getText()+ "\n";
-		result = result + "tests_branch_name=" + testBranchText.getText()+ "\n";
-		result = result + "dreamhr_branch_name=" + dreamBrancText.getText()+ "\n";
-		result = result + "antweety_branch_name=" + antweetyBranchText.getText()+ "\n";
-		result = result + "hudson_ear_folder=" + hudsonEarText.getText()+ "\n";
-		result = result + "ear_build_file=" + earBuildText.getText()+ "\n";
-		result = result + "run_jacoco=" + runCheck.getText()+ "\n";
-		result = result + "build_tools=" + buildCheck.getText()+ "\n";
-		result = result + "upload_languages=" + uploadCheck.getText()+ "\n";
-		result = result + "reason=" + reasonArea.getText()+ "\n";
-		result = result + "notes=" + notesArea.getText()+ "\n";
-		result = result + "build_jeetah=" + jeetahCheck.getText()+ "\n";
-		result = result + "ear_lang=" + langCombo.getValue()+ "\n";
-		result = result + "version_update=" + versionCheck.getText()+ "\n";
-		result = result + "ear.with.antweety=" + earWithCheck.getText();
+		result = result + "prod_branch_name=" + prodBranchText.getText() + "\n";
+		result = result + "tests_branch_name=" + testBranchText.getText() + "\n";
+		result = result + "dreamhr_branch_name=" + dreamBrancText.getText() + "\n";
+		result = result + "antweety_branch_name=" + antweetyBranchText.getText() + "\n";
+		result = result + "hudson_ear_folder=" + hudsonEarText.getText() + "\n";
+		result = result + "ear_build_file=" + earBuildText.getText() + "\n";
+		result = result + "run_jacoco=" + runCheck.getText() + "\n";
+		result = result + "build_tools=" + buildCheck.getText() + "\n";
+		result = result + "upload_languages=" + uploadCheck.getText() + "\n";
+		result = result + "reason=" + reasonArea.getText() + "\n";
+		result = result + "notes=" + notesArea.getText() + "\n";
+		result = result + "build_jeetah=" + jeetahCheck.getText() + "\n";
+		result = result + "ear_lang=" + langCombo.getValue() + "\n";
+		result = result + "version_update=" + versionCheck.getText() + "\n";
+		result = result + "ear.with.antweety=" + earWithCheck.getText() + "\n";
 
 		return result;
 	}
-	
-	
+
 	public void writeNewFile(String text) {
 
-		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("hudson.params", true)))) {
-			
-			
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("hudson.params", false)))) {
+
 			out.write(text);
-			
-//			out.println("jaf_branch_name=" + jafBranchText.getText());
-//			out.println("prod_branch_name=" + prodBranchText.getText());
-//			out.println("tests_branch_name=" + testBranchText.getText());
-//			out.println("dreamhr_branch_name=" + dreamBrancText.getText());
-//			out.println("antweety_branch_name=" + antweetyBranchText.getText());
-//			out.println("hudson_ear_folder=" + hudsonEarText.getText());
-//			out.println("ear_build_file=" + earBuildText.getText());
-//			out.println("run_jacoco=" + runCheck.getText());
-//			out.println("build_tools=" + buildCheck.getText());
-//			out.println("upload_languages=" + uploadCheck.getText());
-//			out.println("reason=" + reasonArea.getText());
-//			out.println("notes=" + notesArea.getText());
-//			out.println("build_jeetah=" + jeetahCheck.getText());
-//			out.println("ear_lang=" + langCombo.getValue());
-//			out.println("version_update=" + versionCheck.getText());
-//			out.println("product_version=" + productVersion.getText());
-//			out.println("ear.with.antweety=" + earWithCheck.getText());
-			
+
 
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
